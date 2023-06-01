6c3eabe6150bf9be66233b672f0c013deb709933
tjdeveloper <tjmr1996@gmail.com>
2023-06-01 10:03:20 +0500

    some changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 const pool = require('./pool');
 
-const database = process.env.DB_NAME;
-const password = process.env.DB_PASSWORD;
-const user = process.env.DB_USER;
+const database = process.env.PGDATABASE;
+const password = process.env.PGPASSWORD;
+const user = process.env.PG_USER;
 const PORT = process.env.PORT || 8000;
-const host = process.env.DB_HOST;
+const host = process.env.PGHOST;
 const port = process.env.PGPORT;
 
 pool
